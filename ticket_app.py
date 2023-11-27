bd131ad8da45879bc3fd3dc43233470d889ba659
Richard Nwokoye <RNwokoye@gmail.com>
2023-11-27 15:11:42 -0500

    made changes

--- ticket_app.py
+++ ticket_app.py
@@ -13,7 +13,7 @@ def check_password():
             user_nme = st.text_input("Username", key="username")
             pass_wrd = st.text_input("Password", type="password", key="password")
             st.form_submit_button("Log in", on_click=password_entered)
-            return user_nme
+            return True, user_nme
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
