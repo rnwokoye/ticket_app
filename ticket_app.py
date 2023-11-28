4eaa9f55de3b5560a0848cb00195cb9c0db5c08d
Richard Nwokoye <RNwokoye@gmail.com>
2023-11-27 19:56:53 -0500

    new file for test

--- ticket_app.py
+++ ticket_app.py
@@ -121,9 +121,6 @@ from streamlit_login_auth_ui.widgets import __login__
 #     create_offense()
 
 
-import streamlit as st
-from streamlit_login_auth_ui.widgets import __login__
-
 __login__obj = __login__(
     auth_token="courier_auth_token",
     company_name="Shims",
